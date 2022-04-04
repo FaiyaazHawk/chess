ec074a279847c4f7988759feef314a484ebaa587
FaiyaazHawk <faiyaaz.haque90@gmail.com>
2022-04-03 22:10:06 -0400

    initialize board with nil

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,14 @@
 class Board
+    attr_accessor :grid
     
+    def initialize
+        @grid = Array.new(8){Array.new(8, nil)}
+    end
+
+    def draw_board
+        pp grid
+    end
 end
+
+b = Board.new
+b.draw_board
