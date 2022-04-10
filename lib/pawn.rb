e6ca60dccc2e9fc62ad49ea5e3168b893ed421b6
FaiyaazHawk <faiyaaz.haque90@gmail.com>
2022-04-10 17:51:08 -0400

    color distinction for start

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -13,17 +13,17 @@ class Pawn < Piece
         one_forward = position[0] + forward_direction, position[1]
         two_forward = position[0] + (forward_direction * 2), position[1]
         #black start
-        if position[0] == 1 && board.empty?(one_forward[0], one_forward[1]) && board.empty?(two_forward[0], two_forward[1])
+        if position[0] == 1 && board.empty?(one_forward[0], one_forward[1]) && board.empty?(two_forward[0], two_forward[1]) && color == :black
             moves << two_forward
         end
-        if position[0] == 1 && board.empty?(one_forward[0], one_forward[1])
+        if position[0] == 1 && board.empty?(one_forward[0], one_forward[1]) && color == :black
             moves << one_forward
         end
         #white start
-        if position[0] == 6 && board.empty?(one_forward[0], one_forward[1]) && board.empty?(two_forward[0], two_forward[1])
+        if position[0] == 6 && board.empty?(one_forward[0], one_forward[1]) && board.empty?(two_forward[0], two_forward[1]) && color == :white
             moves << two_forward
         end
-        if position[0] == 6 && board.empty?(one_forward[0], one_forward[1])
+        if position[0] == 6 && board.empty?(one_forward[0], one_forward[1]) && color == :white
             moves << one_forward
         end
 
